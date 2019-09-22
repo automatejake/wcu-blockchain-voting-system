86e56db37301fc3f8fb40c0ddc70f7956e83ae95
jneubaum <jacob@neubaum.com>
2019-09-22 08:29:24 -0400

    fixed url in assignment 4.txt

--- Learning-Resources/Jacob/Assignment-4/PoS.go
+++ Learning-Resources/Jacob/Assignment-4/PoS.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"time"
 	// "bufio"
 	// "encoding/hex"
 	// "encoding/json"
@@ -39,6 +40,37 @@ func calculateHash(block Block) string {
 }
 
 // create a new block using previous block's hash
+func generateBlock(oldBlock Block, message string) (Block, error) {
+	t := time.Now()
+	var newBlock Block
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Timestamp = t.String()
+	newBlock.Message = message
+	newBlock.PrevHash = oldBlock.Hash
+	newBlock.Hash = calculateHash(newBlock)
+	return newBlock, nil
+}
+
+// make sure block is valid by checking index, and comparing the hash of the previous block
+func isBlockValid(newBlock, oldBlock Block) bool {
+	if newBlock.Index != oldBlock.Index+1 {
+		return false
+	}
+	if newBlock.PrevHash != oldBlock.Hash {
+		return false
+	}
+	if newBlock.Hash != calculateHash(newBlock) {
+		return false
+	}
+	return true
+}
+
+// make sure the chain we're checking is longer than the current blockchain
+func replaceChain(newBlocks []Block) {
+	if len(newBlocks) > len(Blockchain) {
+		Blockchain = newBlocks
+	}
+}
 
 func main() {
 	fmt.Println("Proof of stake chain")
