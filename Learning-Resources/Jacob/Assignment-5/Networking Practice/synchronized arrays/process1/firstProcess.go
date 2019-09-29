9d13fe514ea1de72b7f6fd60938f168db8b58b10
jneubaum <jacob@neubaum.com>
2019-09-29 19:49:17 -0400

    tcp server

--- Learning-Resources/Jacob/Assignment-5/Networking Practice/synchronized arrays/process1/firstProcess.go
+++ Learning-Resources/Jacob/Assignment-5/Networking Practice/synchronized arrays/process1/firstProcess.go
@@ -2,8 +2,58 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"os"
 )
 
+type Block struct {
+	index     int
+	randomNum int
+}
+
+var SyncBlockchain []Block
+var index int
+
+func handleConn(conn net.Conn) {
+	newValue := rand.Intn(100)
+	var tempBlock Block
+
+	tempBlock.index = index
+	tempBlock.randomNum = newValue
+
+	fmt.Println("New value is ", newValue)
+	SyncBlockchain = append(SyncBlockchain, tempBlock)
+	fmt.Println(SyncBlockchain)
+	index++
+
+	conn.Write([]byte(string("\nhelodocmderefrf\n")))
+	conn.Close()
+}
+
 func main() {
-	fmt.Println("hello")
+	index = 0
+	port := ":1200"
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", port)
+	checkError(err)
+
+	server, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go handleConn(conn)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
 }
