78f425eb6aec07c5416469f2fbfd709b60ce8cf9
jneubaum <jacob@neubaum.com>
2019-10-01 15:09:09 -0400

    finished shell script

--- Learning-Resources/Jacob/Assignment-5/Networking Practice/synchronized arrays/processes1/process1.go
+++ Learning-Resources/Jacob/Assignment-5/Networking Practice/synchronized arrays/processes1/process1.go
@@ -1,11 +1,111 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"os"
+	"io"
+	"log"
+	"net"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
+type Block struct {
+	Index   int
+	Message string
+}
+
+var Clients []int
+var SyncBlockchain []Block
+var index int
+
+/*************
+*
+*  Two primary threads with child processes:
+*	1 - client, search for peers on the network and listen in on them
+*		a - each peer found opens a new listening process
+*		  - each listening process adds new things to the chain
+*	2 - server, listen for incoming connections
+*		a - each client that connects launches a listening process
+*		  - whenever, there is new data in the array, each listening process sends data to the listener
+*
+*************/
+
+func foundPeer(conn net.Conn) {
+	fmt.Println("found peer!")
+
+	message, _ := bufio.NewReader(conn).ReadString('\n')
+	fmt.Print("Message from server: " + message)
+
+}
+
+func listenConnections() {
+	port := ":1200"
+	server, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Server listening on port", port)
+
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+
+	defer fmt.Println("Client disconnected")
+	defer conn.Close()
+
+	fmt.Println("Client connected")
+	scanner := bufio.NewScanner(conn)
+
+	go broadcastChain()
+
+	for scanner.Scan() {
+		io.WriteString(conn, "\nEnter a message to write to the block:  ")
+		message := scanner.Text()
+		var newBlock Block
+		newBlock.Index = index
+		newBlock.Message = string(message)
+		SyncBlockchain = append(SyncBlockchain, newBlock)
+		spew.Println(SyncBlockchain)
+		index++
+	}
+}
+
+func broadcastChain() {
+	for {
+		time.Sleep(3 * time.Second)
+
+	}
+}
+
 func main() {
-	a := os.Getenv("PORT")
-	fmt.Println("hello from process", a, ".go")
+	index = 0
+	port := 7001
+
+	go listenConnections()
+
+	//Discovering peers, there are 65,535 ports on a computer
+	for {
+
+		conn, _ := net.Dial("tcp", "127.0.0.1:"+port)
+
+		if conn == nil {
+			fmt.Println(conn, port)
+		} else {
+			go foundPeer(conn)
+		}
+
+		time.Sleep(5 * time.Second)
+	}
+
 }
--- Learning-Resources/Jacob/Assignment-5/Networking Practice/synchronized arrays/spawnProcesses.sh
+++ Learning-Resources/Jacob/Assignment-5/Networking Practice/synchronized arrays/spawnProcesses.sh
@@ -1,12 +1,24 @@
 read -p 'Enter the number of Processes: ' num
-
-# touch "process1.go"
+runProcesses=false
+while true; do
+    read -p "Do you wish to run processes?" yn
+    case $yn in
+        [Yy]* ) runProcesses=true; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
 for ((a = 0 ; a < $num; a++ ));
 do
     mkdir "processes$a"
     cp process1.go "processes$a/process$a.go"
     echo -e "PORT=700$a" >> "processes$a/.env"
-    go run "processes$a/process$a.go" &
+    if $runProcesses ; then
+        echo "starting process $a on port 700$a"
+        go run "processes$a/process$a.go" &
+    fi
+    
+    
 done
 
