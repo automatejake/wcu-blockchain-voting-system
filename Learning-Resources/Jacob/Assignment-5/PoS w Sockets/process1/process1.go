8655d948f3220ffce9133fbc1c1bc1af8da4940d
jneubaum <jacob@neubaum.com>
2019-10-09 10:25:09 -0400

    copied files to pos w sockets dir

---
+++ Learning-Resources/Jacob/Assignment-5/PoS w Sockets/process1/process1.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/joho/godotenv"
+)
+
+type Block struct {
+	Index   int
+	Message string
+}
+
+type Peer struct {
+	Port   int
+	Socket net.Conn
+}
+
+var Peers []Peer
+var Nodes = make(map[int]bool)
+
+//both of these should be channels or use mutex for writing to them
+var Blockchain []Block
+var index int
+
+/*************
+*
+*  Two primary threads with child processes:
+*	1 - client, search for Clients on the network and listen in on them
+*		a - each peer found opens a new listening process
+*		  - each listening process adds new things to the chain
+*	2 - server, listen for incoming Sockets
+*		a - each client that connects launches a listening process
+*		  - whenever, there is new data in the array, each listening process sends data to the listener
+*
+*************/
+
+/******* LISTENING PROCESS *******/
+func listenSockets() {
+	port := ":" + os.Getenv("PORT")
+	server, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Server listening on port", port)
+
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go peerProcess(conn)
+	}
+}
+
+/***************************************************************/
+
+/*** PEER PROCESS ***/
+func peerProcess(conn net.Conn) {
+
+	defer fmt.Println("TCP Connection Ended")
+	defer conn.Close()
+
+	fmt.Println("New TCP Connection")
+
+	//listen for new connection types
+	var buf [512]byte
+
+	for {
+
+		msgLength, err := conn.Read(buf[0:])
+
+		if err != nil {
+			return
+		}
+
+		if string(buf[0:7]) == "connect" {
+
+			port, err := strconv.Atoi(string(buf[8:msgLength]))
+			if err == nil {
+				Nodes[port] = true
+				peer := Peer{port, conn}
+				Peers = append(Peers, peer)
+			}
+
+		} else if string(buf[0:9]) == "syncChain" {
+
+			fmt.Println("syncing chain ", msgLength)
+
+		} else if string(buf[0:7]) == "message" {
+			// after connecting via "nc localhost [port]"
+			// write to chain with "message [message inserted here]"
+			tempBlock := Block{index, string(buf[8 : msgLength-1])}
+			Blockchain = append(Blockchain, tempBlock)
+			index++
+
+			//broadcast message to all other connected nodes
+			spew.Println(tempBlock)
+			go broadcast(tempBlock)
+
+		} else if string(buf[0:9]) == "broadcast" {
+			tmpbuff := bytes.NewBuffer(buf[10:msgLength])
+			tmpstruct := new(Block)
+			gobobj := gob.NewDecoder(tmpbuff)
+			gobobj.Decode(tmpstruct)
+			spew.Println(*tmpstruct)
+
+		} else {
+			fmt.Println(msgLength, string(buf[0:10]), buf[0:10])
+		}
+	}
+
+}
+
+func broadcast(tempBlock Block) {
+	// creates an encoder object
+	buf := new(bytes.Buffer)
+	gobobj := gob.NewEncoder(buf)
+	gobobj.Encode(tempBlock)
+
+	for _, element := range Peers {
+		element.Socket.Write(append([]byte("broadcast "), buf.Bytes()...))
+	}
+}
+
+/***************************************************************/
+
+/****** Discovery Process ******/
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	ignore, _ := strconv.Atoi(os.Getenv("PORT"))
+	Nodes[ignore] = true
+
+	index = 0
+
+	go listenSockets()
+
+	//Discovering Clients, there are 65,535 ports on a computer
+	//I am using ports 7000-7020
+	for {
+		for port := 7000; port <= 7020; port++ {
+
+			if !Nodes[port] {
+				currentPort := strconv.Itoa(port)
+				conn, _ := net.Dial("tcp", "127.0.0.1:"+currentPort)
+				if conn != nil {
+					peer := Peer{port, conn}
+					Peers = append(Peers, peer)
+					Nodes[port] = true
+					conn.Write([]byte("connect " + strconv.Itoa(ignore)))
+					go peerProcess(conn)
+				}
+
+			}
+
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
+
+}
+
+/******************************************************************************/
---
+++ Learning-Resources/Jacob/Assignment-5/PoS w Sockets/spawnProcesses.sh
@@ -0,0 +1,26 @@
+num=10
+
+# read -p 'Enter the number of Processes: ' num
+runProcesses=false
+# while true; do
+#     read -p "Do you wish to run processes?" yn
+#     case $yn in
+#         [Yy]* ) runProcesses=true; break;;
+#         [Nn]* ) break;;
+#         * ) echo "Please answer yes or no.";;
+#     esac
+# done
+
+for ((a = 0 ; a < $num; a++ ));
+do
+    mkdir "process$a"
+    cp process.go "process$a/process$a.go"
+    echo -e "PORT=700$a" >> "process$a/.env"
+    if $runProcesses ; then
+        echo "starting process $a on port 700$a"
+        # go run "process$a/process$a.go" &
+    fi
+    
+
+done
+
